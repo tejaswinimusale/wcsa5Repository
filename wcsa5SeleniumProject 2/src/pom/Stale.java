ece224eb6141e8e9676148f44eab8788ab866d30
ADMIN <ADMIN@DESKTOP-7KBD9CU>
2023-07-12 20:05:13 +0530

    commit

--- wcsa5SeleniumProject 2/src/pom/Stale.java
+++ wcsa5SeleniumProject 2/src/pom/Stale.java
@@ -8,25 +8,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Stale {
-
-	public static void main(String[] args) {
-		System.setProperty("webdriver.firefox.driver", "./drivers.FireFoxDriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		
-		driver.get("http://not_for_you/login.do");
-		
-		WebElement usnTB = driver.findElement(By.name("username"));
-		usnTB.sendKeys("admin");
-		WebElement passTB = driver.findElement(By.name("pwd"));
-		passTB.sendKeys("manager");
-		
-		//Thread.sleep(2000);
-		driver.navigate().refresh();
-		usnTB.clear();
-		
-
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+	    WebDriver	driver=new ChromeDriver();
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	    driver.get("http://laptop-knl22f4g/login.do");
+	    
+	    WebElement usnTB = driver.findElement(By.name("username"));
+	    //usnTB.sendKeys("admin");
+	    WebElement pssTB = driver.findElement(By.name("pwd"));
+	  //  pssTB.sendKeys("manager");
+	    
+	 //   Thread.sleep(2000);
+	    driver.navigate().refresh();
+	    usnTB.sendKeys("admin");
+	    
+	    
 	}
 
 }
